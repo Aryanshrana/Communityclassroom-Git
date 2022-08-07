7a935b50ecf4be4ad977025c688e739a4f15c5bd
Aryansh <500093038@stu.upes.ac.in>
2022-08-07 12:50:27 +0530

    java program modified

--- first.java
+++ first.java
@@ -1,10 +1,11 @@
 import java.util.*;
-//program to generate random integers between 1 to 666666"
+//program to generate random integers from 1 to 6
 public class Main{
 	public static void main(String[] args){
 		Random random = new Random();
 			int x = random.nextInt(6)+1;
-			System.out.println("random number between 1 to 6 is "+x);
+			System.out.println("hello user");
+			System.out.println("random number from 1 to 6 is "+x);
 	}
 }
 
