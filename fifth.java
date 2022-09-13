866614822a5e3307401fb9ac3ad59cf273fd702e
Aryansh <500093038@stu.upes.ac.in>
2022-09-13 14:12:48 +0530

    new feature added

--- fifth.java
+++ fifth.java
@@ -12,12 +12,13 @@ class Targaryen import Dragon
 		System.out.println("master name = "+"Daemon Targaryen");
 		System.out.print(" master of dragonstones");
 		System.out.println("Dragon color"+" red");
+		System.out.println("caraxes is also known as blood wyrm");
 	}
 }
 class Main{
 	public static void main(String[] args){
 		Targaryen t = new Targaryen();
-		t.Dragondetails("Craxes");
+		t.Dragondetails("Caraxes");
 	}
 }
 
